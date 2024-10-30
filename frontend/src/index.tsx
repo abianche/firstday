640adb95cfc70b22be3aa46533bb36c78a5d4b96
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2024-10-30 20:13:48 +0100

    refactor: migarte to react18 root render

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 const App: React.FC = () => <h1>Welcome to FirstDay!</h1>;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container); // createRoot(container!) if you're certain container is not null
+  root.render(<App />);
+}
