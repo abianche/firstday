c182b4aa17aed62a03778012f467ddb0ce3fb922
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2024-11-04 20:12:05 +0100

    feat: switch to mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Button from "@mui/material/Button";
+import RocketIcon from "@mui/icons-material/rocket";
+
 export default function Home() {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Welcome to FirstDay</h1>
+      <Button variant="contained" endIcon={<RocketIcon />}>
+        Send
+      </Button>
     </div>
   );
 }
