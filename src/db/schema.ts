c2b1d4d75a3370c4bb92cf06e81d98d434dc488e
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2024-11-09 18:08:14 +0100

    add drizzle commands, drizzle-zod, move docker files

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,11 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
 });
+
+export const insertUserSchema = createInsertSchema(users);
+export const selectUserSchema = createSelectSchema(users);
