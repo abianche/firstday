68732db96159e05729610a06b9e5dbead9de94a7
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2024-11-15 00:16:53 +0100

    wip add layout

--- src/app/add/layout.tsx
+++ src/app/add/layout.tsx
@@ -2,7 +2,7 @@
 
 import Header from "@/components/Header";
 import SideMenu from "@/components/SideMenu";
-import { Box, Stack } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 
 export default function AddLayout({
   children,
@@ -13,22 +13,30 @@ export default function AddLayout({
     <Box sx={{ display: "flex" }}>
       <SideMenu />
       <Box
-        component="main"
         sx={{
           flexGrow: 1,
           overflow: "auto",
         }}
       >
         <Stack
-          spacing={2}
+          spacing={1}
           sx={{
-            alignItems: "center",
             mx: 3,
-            pb: 5,
-            mt: 0,
+            pb: 3,
           }}
         >
-          <Header />
+          <Stack
+            direction="row"
+            useFlexGap
+            spacing={2}
+            alignItems={"flex-start"}
+            justifyContent={"space-between"}
+          >
+            <Header />
+            <Button variant="contained" sx={{ mt: 2 }}>
+              Save
+            </Button>
+          </Stack>
           {children}
         </Stack>
       </Box>
