0bd6d094afeec5925f9e76866194f5fed193bebf
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2024-11-17 18:54:52 +0100

    add licenses generation and page

---
+++ src/app/licenses/layout.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Header from "@/components/Header";
+import SideMenu from "@/components/SideMenu";
+import { Box, Stack } from "@mui/material";
+
+export default function LicenseLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <SideMenu />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          overflow: "auto",
+        }}
+      >
+        <Stack
+          spacing={1}
+          sx={{
+            alignItems: "center",
+            mx: 3,
+            pb: 3,
+          }}
+        >
+          <Header />
+          {children}
+        </Stack>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/app/licenses/page.tsx
@@ -0,0 +1,96 @@
+import LinkIcon from "@mui/icons-material/Link";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+  Typography,
+} from "@mui/material";
+import fs from "fs";
+import path from "path";
+import * as R from "ramda";
+
+interface IPackageLicenseMeta {
+  author: string;
+  license: string;
+  licenseText: string;
+  name: string;
+  noticeText?: string;
+  repository: string;
+  source: string;
+  version: string;
+}
+
+export default async function LicensesPage() {
+  // Read the oss-licenses.json file from the .next folder
+  const filePath = path.join(process.cwd(), ".next", "oss-licenses.json");
+  const fileContents = fs.readFileSync(filePath, "utf-8");
+  const licenses: IPackageLicenseMeta[] = JSON.parse(fileContents);
+  const filteredLicenses = R.reject(
+    (license) => R.isNil(license.licenseText),
+    licenses
+  );
+
+  console.info(`Retrieved a total of ${R.length(filteredLicenses)} licenses.`);
+
+  function isValidUrl(input: string, name: string): boolean {
+    try {
+      new URL(input); // Built-in URL class
+      return true;
+    } catch {
+      console.info(`Cannot parse "${input}" into URL for package "${name}".`);
+      return false;
+    }
+  }
+
+  return (
+    <Box sx={{ p: 2, width: "100%", margin: "auto" }}>
+      <Typography variant="h1" gutterBottom>
+        3rd party open source licenses
+      </Typography>
+
+      <Stack spacing={2} direction="column">
+        {R.map(
+          (license) => (
+            <Card key={`${license.name}-${license.version}`}>
+              <CardHeader
+                action={
+                  isValidUrl(license.repository, license.name) ? (
+                    <Button
+                      startIcon={<LinkIcon />}
+                      size="small"
+                      href={license.repository}
+                      target="_blank"
+                      rel="noopener"
+                      aria-label="Navigate to repository"
+                    >
+                      Repository
+                    </Button>
+                  ) : null
+                }
+                title={license.name}
+                subheader={`${license.version}${
+                  license.author ? ` - ${license.author}` : ""
+                }`}
+              />
+              <CardContent>
+                <Typography
+                  variant="body1"
+                  component="pre"
+                  sx={{
+                    overflowX: "auto",
+                  }}
+                >
+                  {license.licenseText}
+                </Typography>
+              </CardContent>
+            </Card>
+          ),
+          filteredLicenses
+        )}
+      </Stack>
+    </Box>
+  );
+}
