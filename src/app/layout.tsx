cbdb2e212b0a3d58ff17d5b8071b3267b82546d9
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2024-11-19 22:54:56 +0100

    update metadata head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,21 @@ export default function RootLayout({
   return (
     <StoreProvider>
       <html lang="en" suppressHydrationWarning>
+        <head>
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <meta name="apple-mobile-web-app-title" content="First Day" />
+        </head>
         <body>
           <Analytics />
           <SpeedInsights />
