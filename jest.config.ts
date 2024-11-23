a29b269a56a3d221d190ba7ec059ee838deb87fd
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2024-11-23 13:45:37 +0100

    fixed jest config

--- jest.config.ts
+++ jest.config.ts
@@ -131,12 +131,10 @@ const config: Config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
+  // rootDir: "./",
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  // roots: ["<rootDir>"],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -164,8 +162,8 @@ const config: Config = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.[jt]s?(x)",
-    "<rootDir>/src/**/?(*.)+(spec|test).[tj]s?(x)",
+    "<rootDir>/src/**/__tests__/**/*.[jt]s",
+    "<rootDir>/src/**/*.(spec|test).[jt]s",
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
