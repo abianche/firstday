e380ca2e92a2f03b6f6a9f041eca7cf6007f22ab
abianche <alessio.bianchetti@gmail.com>
2024-11-23 22:01:12 +0100

    removed custom theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,14 @@
 "use client";
 
-import { colorSchemes, typography, shape } from "./theme/themePrimitives";
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
-  // defaultColorScheme: "dark",
   cssVariables: {
     colorSchemeSelector: "data-mui-color-scheme",
   },
-  colorSchemes,
-  typography,
-  shape,
+  colorSchemes: {
+    dark: true,
+  },
 });
 
 export default theme;
