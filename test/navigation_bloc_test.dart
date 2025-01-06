279b145f08910601cb0cd499ccf69522b2c0be1d
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2025-01-06 22:01:01 +0100

    test: add unit tests for NavigationBloc

---
+++ test/navigation_bloc_test.dart
@@ -0,0 +1,30 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:firstday/bloc/navigation_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('NavigationBloc', () {
+    test('initial state is NavigationInitial()', () {
+      final bloc = NavigationBloc();
+      expect(bloc.state, equals(const NavigationInitial()));
+    });
+
+    blocTest<NavigationBloc, NavigationState>(
+      'emits [NavigationState(screenIndex: 1)] when NavigationIndexChanged(1) is added',
+      build: () => NavigationBloc(),
+      act: (bloc) => bloc.add(const NavigationIndexChanged(1)),
+      expect: () => <NavigationState>[
+        const NavigationState(screenIndex: 1),
+      ],
+    );
+
+    blocTest<NavigationBloc, NavigationState>(
+      'emits [NavigationState(screenIndex: 2)] when NavigationIndexChanged(2) is added',
+      build: () => NavigationBloc(),
+      act: (bloc) => bloc.add(const NavigationIndexChanged(2)),
+      expect: () => <NavigationState>[
+        const NavigationState(screenIndex: 2),
+      ],
+    );
+  });
+}
